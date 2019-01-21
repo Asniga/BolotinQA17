87422b970ab5d869debeef215a8e8c5c45ae588a
Asniga <asniga@gmail.com>
2019-01-21 18:24:07 +0200

    Created module "First project"

---
+++ TelRan/src/test/java/OpenBrowser.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+WebDriver wd;
+
+@BeforeClass
+    public void setUp (){
+    wd=new ChromeDriver();
+    }
+
+@Test
+public void chromeBrowserTest(){
+    wd.get("https://www.google.ru/");
+}
+
+@AfterClass
+    public void tearDown() throws InterruptedException {
+    Thread.sleep(3000);
+    wd.quit();
+}
+}
+
